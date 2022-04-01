70bef76b28d220df33cc9909916623d465408112
Vladislav <amcors21@gmail.com>
2022-04-01 20:06:14 +0300

    Added program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 0;
+string b;
+Console.Clear();
+System.Console.WriteLine("Введите размер массива: ");
+b = Console.ReadLine();
+a = Convert.ToInt32(b);
+
+string [] arr = new string [a];
+for (int i = 0; i < arr.Length; i++)
+{
+    System.Console.WriteLine("Введите строку: ");
+    b = Console.ReadLine();
+    arr[i] = b;
+}
+System.Console.WriteLine("Заданный массив имеет вид: ");
+for (int i = 0; i < arr.Length; i++)
+{
+    System.Console.Write($"{arr[i]} ");
+}
+Console.Clear();
+System.Console.WriteLine("Массив из строк, длинна которых меньше либо равна 3 символа: ");
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i].Length <= 3) System.Console.WriteLine(arr[i]);
+}
