1c94e42ffc7e873e219f1867b9eb04c7e0e925a2
Vladislav <amcors21@gmail.com>
2022-04-03 15:00:23 +0300

    Change prog

--- Program.cs
+++ Program.cs
@@ -12,14 +12,29 @@ for (int i = 0; i < arr.Length; i++)
     b = Console.ReadLine();
     arr[i] = b;
 }
+
 System.Console.WriteLine("Заданный массив имеет вид: ");
 for (int i = 0; i < arr.Length; i++)
 {
-    System.Console.Write($"{arr[i]} ");
+    System.Console.WriteLine(arr[i]);
 }
-Console.Clear();
-System.Console.WriteLine("Массив из строк, длинна которых меньше либо равна 3 символа: ");
+/////////////////////////////////////
+int c = 0, d = 0;
 for (int i = 0; i < arr.Length; i++)
 {
-    if (arr[i].Length <= 3) System.Console.WriteLine(arr[i]);
+    if (arr[i].Length <= 3) c += 1;
+}
+string [] arr1 = new string [c];
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i].Length <= 3)
+    {
+        arr1[d] = arr[i];
+        d += 1;
+    }
+}
+System.Console.WriteLine("массив с длинной строк 3 и менее элементов имеет вид: ");
+for (int i = 0; i < arr1.Length; i++)
+{
+    System.Console.WriteLine(arr1[i]);
 }
